5f2d80ee4b9893ccfb7f6072ae77b625849e1b54
Flatiron School <flatironschool@Drew-2.local>
2016-10-13 13:25:39 -0400

    Completed!

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   req = Rack::Request.new(env)
+  #
+  #
+  # end
+  get "/name" do
+    "My name is Drew"
+  end
+
+  get "/hometown" do
+    "My hometown is Chapel Hill"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is All-Star by SmashMouth"
+  end
+
+
 end
